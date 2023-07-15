a2b2fedc9b0fe0b478e475b152879ecfb70d76c5
Mathewvanh <vanh.mathew@gmail.com>
2023-07-15 14:10:02 +0800

    You have to do it destructively on account of the limitations of the java iterator.

--- src/IteratorOfIterators.java
+++ src/IteratorOfIterators.java
@@ -1,34 +1,30 @@
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 public class IteratorOfIterators implements Iterator{
-    private List<Iterator<Integer>>L;
-    private int index;
-    private int element;
-    public int size;
+    private LinkedList<Integer>L;
+
     public IteratorOfIterators(List<Iterator<Integer>> a){
-        L=a;
-        index=0;
-        element=0;
-        size=L.size();
+        L = new LinkedList<>();
+        while(!a.isEmpty()){
+            Iterator<Integer> curr = a.remove(0);
+            if(curr.hasNext()){
+                L.add(curr.next());
+                a.add(curr);
+            }
+        }
     }
     @Override
     public boolean hasNext(){
-        if(index<-1 || index>L.size()-1)return false;//when we reach the end
-        return true;
+        return !L.isEmpty();
     }
     @Override
     public Integer next(){
-        if(!hasNext())throw new NoSuchElementException();
-        while(!L.get(index).hasNext()){
-            index++;
-            if(index==size && !L.get(index).hasNext())
-            if(index==size)index=0;
-        }
-        Integer returnItem = L.get(index).next();
-        index++;
-        if(index==size)index=0;
-        return returnItem;
+        if(!hasNext()){
+            throw new NoSuchElementException();
+        };
+        return L.removeFirst();
     }
 }
